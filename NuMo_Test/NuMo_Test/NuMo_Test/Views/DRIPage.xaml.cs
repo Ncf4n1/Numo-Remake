30917817d93529b2d38592b1fa6496948895ea7f
Logan Vining <llvinin@hotmail.com>
2019-02-04 11:25:56 -0700

    Added baseline DRI Functionality

--- NuMo_Test/NuMo_Test/NuMo_Test/Views/DRIPage.xaml.cs
+++ NuMo_Test/NuMo_Test/NuMo_Test/Views/DRIPage.xaml.cs
@@ -12,5 +12,20 @@ namespace NuMo_Test.Views
         {
             InitializeComponent();
         }
+
+        async void GetHelpInfo(object sender, EventArgs e)
+        {
+            await DisplayAlert("DRI Information", "Dietary Reference Intakes refers to a set of values that assess your nutrition intake. These values are the recommended minimal values. You may set your own target values based on your needs and goals.", "OK");
+        }
+
+        async void CustomizeDRIs(object sender, EventArgs e)
+        {
+            await DisplayAlert("Customize", "Are you sure you want to customize your recommended Dietary Reference Intakes?", "No", "Yes");
+        }
+
+        async void ResetDRIs(object sender, EventArgs e)
+        {
+            await DisplayAlert("Reset", "Are you sure you want to reset your Dietary Reference Intakes to the minimum recommended values?", "No", "Yes");
+        }
     }
 }
