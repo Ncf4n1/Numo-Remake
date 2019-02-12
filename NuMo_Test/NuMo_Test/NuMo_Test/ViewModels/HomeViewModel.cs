62defb15d27e0076a86c53f8201f34b5a1206d52
Logan Vining <llvinin@hotmail.com>
2019-02-12 08:39:28 -0700

    Settings page works with DRI and with Create Food, Recipe, Add Food

--- NuMo_Test/NuMo_Test/NuMo_Test/ViewModels/HomeViewModel.cs
+++ NuMo_Test/NuMo_Test/NuMo_Test/ViewModels/HomeViewModel.cs
@@ -17,7 +17,7 @@ namespace NuMo_Test.ViewModels
 
         public HomeViewModel()
         {
-            Title = "Home";
+            Title = "My Day";
             Items = new ObservableCollection<Item>();
             LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
 
