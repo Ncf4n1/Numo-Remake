e374c9e2c4da071842f105d17b1769e33ab771dd
Logan Vining <llvinin@hotmail.com>
2019-03-06 17:46:18 -0700

    Added Keto Page

---
+++ NuMo_Test/NuMo_Test/NuMo_Test/Views/KetoPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NuMo_Test.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class KetoPage : ContentPage
+	{
+        public KetoPage()
+        {
+            InitializeComponent();
+
+            this.Title = "Keto Index Information";
+        }
+
+        public void calculateGKI(object sender, EventArgs args)
+        {
+            try
+            {
+                double index = (double.Parse(glucoseEntry.Text) / 18) / double.Parse(ketoEntry.Text);
+                Label indexText = new Label { Text = "Your Glucose Ketone Index is " + index.ToString("F"), FontSize = 21, TextColor = Color.FromHex("#00A4A7") };
+
+                ketoStack.Children.Add(indexText);
+            }
+            catch (Exception e)
+            {
+                DisplayAlert("Invalid Input", "Please enter a number", "OK");
+            }
+        }
+    }
+}
